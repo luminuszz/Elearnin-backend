558f5fa2e670f59e4dbee2ef99184d61e88f3a26
luminuszz <luminuszz43@gmail.com>
2020-11-07 18:58:17 -0300

    s

--- src/modules/courses/courses.module.ts
+++ src/modules/courses/courses.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { User } from '../users/entities/user.entity'
 import { UserModule } from '../users/users.module'
 import { CoursesController } from './controllers/courses.controller'
 import { Course } from './entities/course.entity'
 import { CoursesService } from './services/courses.service'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Course, User]), UserModule],
+  imports: [TypeOrmModule.forFeature([Course]), UserModule],
   controllers: [CoursesController],
   providers: [CoursesService],
 })
