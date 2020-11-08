ca762ade604a333e210237527fc59b9bb10384d9
luminuszz <luminuszz43@gmail.com>
2020-11-07 22:20:26 -0300

    admin seed

--- src/database/migrations/1604797403945-PopulateAdmins.ts
+++ src/database/migrations/1604797403945-PopulateAdmins.ts
@@ -1,16 +1,18 @@
 import { User } from 'src/modules/users/entities/user.entity'
 import { MigrationInterface, QueryRunner } from 'typeorm'
+import { hash } from 'bcrypt'
 
 export class PopulateAdmins1604797403945 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     const userRepository = queryRunner.manager.getRepository(User)
+    const passwordHash = await hash(123456, 10)
 
     const AdminSeed = userRepository.create({
       name: 'adminTeste',
       city: 'Salvador',
       state: 'BA',
       email: 'adminTeste@gmail.com',
-      passwordHash: '123456',
+      passwordHash,
       zipCode: '544545',
       role: 'admin',
     })
